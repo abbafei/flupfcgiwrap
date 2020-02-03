d065f6802140bb6d168c0c44a33ed80ec0d110ab
Abbafei <mail+git@abbafei.com>
2020-02-02 19:32:36 -0500

    B"H add python 3 support, etc.

--- flupfcgiwrap.py
+++ flupfcgiwrap.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 #B"H.
-'''for use with fcgiwrap and the like'''
-import imp
+'''like fcgiwrap, for wsgi applications. for use with spawn-fcgi and the like.'''
+import sys
+if sys.version_info.major < 3:
+    import imp
+else:
+    import importlib.util
+    import importlib.machinery
+
+
+def _imported_source(file_path, out_module_name):
+    if sys.version_info.major < 3:
+        return imp.load_source(out_module_name, file_path)
+    else:
+        #spec = importlib.util.spec_from_file_location(out_module_name, file_path) # doesn't work for files with unrecognized file extensions
+        # via https://stackoverflow.com/a/19011259
+        loader = importlib.machinery.SourceFileLoader(out_module_name, file_path)
+        spec = importlib.util.spec_from_loader(loader.name, loader)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
 
 
 if __name__ == '__main__':
@@ -16,6 +34,6 @@ if __name__ == '__main__':
         sys.stderr.write('usage: {0} <python-file> <app_varname>\n'.format(sys.argv[0]))
         exit(1)
     else:
-        mod = imp.load_source('', sys.argv[1])
+        mod = _imported_source(sys.argv[1], '')
         appy = getattr(mod, sys.argv[2])
         WSGIServer(appy).run()
